dcefdf8b4e2df77b6c18e6d66db18dec631b27f3
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-18 09:53:48 +0200

    fix console log at end

--- js/main.js
+++ js/main.js
@@ -22,18 +22,18 @@ setTimeout(function () {
     console.log(userNumberArray);
     console.log(randomNumberArray);
     let samenumbersCount = 0;
+    let sameNumbers = [];
     for (let i = 0; i < userNumberArray.length;i++) {
         const currentNumber = userNumberArray[i];
-        let sameNumbers = [];
         // console.log(currentNumber);
         if (randomNumberArray[i] === currentNumber) {
             samenumbersCount += 1
             sameNumbers.push(currentNumber);
             // console.log(sameNumbers);
         }
-        console.log("i numeri indovinati sono: "+ samenumbersCount + "e sono i numeri "+ sameNumbers);
         // console.log("i numeri indovinati sono:" + currentNumber);
     }
+    console.log("i numeri indovinati sono: "+ samenumbersCount + " e sono i numeri "+ sameNumbers);
 
     // return userNumberArray
     // console.log(userNumberArray.includes(2));
